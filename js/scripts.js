b949439498d8fef2f0d9520aa4ef78cc7778c74e
Latte <portisroad@gmail.com>
2025-01-12 09:24:10 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -329,17 +329,21 @@ function parseXtreamDetails(link) {
 
 function showToast(message) {
     toastr.options = {
-        "closeButton": true,
-        "debug": false,
-        "newestOnTop": false,
-        "progressBar": true,
-        "positionClass": "toast-bottom-right",
-        "preventDuplicates": false,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000"
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": true,
+  "progressBar": true,
+  "positionClass": "toast-bottom-left",
+  "preventDuplicates": true,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "3000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
     };
     toastr.success(message);
 }
